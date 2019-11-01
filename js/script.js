b15bbbb97fc20378ebe321639326866a48e54247
audaxloot <56904930+audaxloot@users.noreply.github.com>
2019-11-01 03:09:09 -0400

    HTML/CSS Revamp

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let content = `
+<table id="dkptable">
+<tr>
+  <th class="header">Player</th>
+  <th class="header">Class</th>
+  <th class="header">Current</th>
+</tr>
+`
+
+$.ajax({
+  url: 'https://audaxloot.github.io/xml/dkp.xml',
+  type: 'get',
+  dataType: 'xml',
+  success: function(data) {
+    $(data).find('dkpentry').each(function() {
+      let player = $(this).find('player').text()
+      let pclass = $(this).find('class').text().toLowerCase()
+      let current = $(this).find('dkp').text()
+
+      content = content + `
+      <tr>
+        <td class="player">${player}</td>
+        <td class="pclass"><span class="class_text">${pclass}</span><img src="https://wow.zamimg.com/images/wow/icons/large/classicon_${pclass}.jpg"></td>
+        <td class="current">${current}</td>
+      </tr>
+      `
+    })
+    content = content + `</table>`
+    $("#content").append(content)
+    let table = document.getElementById('dkptable')
+    sorttable.makeSortable(table);
+  },
+  error: function() {
+    $('#content').text('Failed to load data!')
+  }
+})
