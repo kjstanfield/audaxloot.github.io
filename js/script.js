252f35f40453327db529c4456449c387ec6d49f3
Kyle S <kjstan@gmail.com>
2019-11-07 00:29:47 -0500

    Working on V2 DKP list

--- js/script.js
+++ js/script.js
@@ -1,36 +1,92 @@
-let content = `
-<table id="dkptable">
-<tr>
-  <th class="header">Player</th>
-  <th class="header">Class</th>
-  <th class="header">Current</th>
-</tr>
-`
+var whTooltips = {
+  colorLinks: true,
+  iconizeLinks: true,
+  renameLinks: true
+};
 
+// MAIN DKP TABLE
 $.ajax({
   url: 'https://audaxloot.github.io/xml/dkp.xml',
   type: 'get',
   dataType: 'xml',
   success: function(data) {
     $(data).find('dkpentry').each(function() {
-      let player = $(this).find('player').text()
-      let pclass = $(this).find('class').text().toLowerCase()
-      let current = $(this).find('dkp').text()
+      let dkp_player = $(this).find('player').text()
+      let dkp_class = $(this).find('class').text().toLowerCase()
+      let dkp_current = $(this).find('dkp').text()
 
-      content = content + `
-      <tr class="entry">
-        <td class="player">${player}</td>
-        <td class="pclass"><span class="class_text">${pclass}</span><img src="https://wow.zamimg.com/images/wow/icons/large/classicon_${pclass}.jpg"></td>
-        <td class="current">${current}</td>
-      </tr>
-      `
+      let new_row = `
+      <tr class="dkp_entry">
+        <td class="row dplayer">${dkp_player}</td>
+        <td class="row dclass"><span class="dtext">${dkp_class}</span><img src="https://wow.zamimg.com/images/wow/icons/large/classicon_${dkp_class}.jpg"></td>
+        <td class="row dcurrent">${dkp_current}</td>
+      </tr>`
+
+      $("#dkp_table tbody").append(new_row)
     })
-    content = content + `</table>`
-    $("#content").append(content)
-    let table = document.getElementById('dkptable')
-    sorttable.makeSortable(table);
   },
   error: function() {
-    $('#content').text('Failed to load data!')
+    $('#dkp_table').text('Failed to load data!')
+  }
+})
+
+
+// LOOTED ITEM HISTORY
+$.ajax({
+  url: 'https://audaxloot.github.io/xml/loot.xml',
+  type: 'get',
+  dataType: 'xml',
+  success: function(data) {
+    $(data).find('lootentry').each(function() {
+      let loot_player = $(this).find('player').text()
+      let loot_itemname = $(this).find('itemname').text()
+      let loot_itemnum = $(this).find('itemnumber').text()
+      let loot_zone = $(this).find('zone').text()
+      let loot_boss = $(this).find('boss').text()
+      let loot_cost = $(this).find('cost').text()
+
+      let new_row = `
+      <tr class="loot_entry">
+        <td class="row lplayer">${loot_player}</td>
+        <td class="row litem"><a href="https://classic.wowhead.com/item=${loot_itemnum}" target="_blank" data-wowhead="item=${loot_itemnum}&domain=classic">${loot_itemname}</a></td>
+        <td class="row lzone">${loot_zone}</td>
+        <td class="row lboss">${loot_boss}</td>
+        <td class="row lcost">${loot_cost}</td>
+      </tr>`
+
+      $("#loot_table tbody").append(new_row)
+    })
+  },
+  error: function() {
+    $('#loot_table').text('Failed to load data!')
+  }
+})
+
+
+// DKP HISTORY
+$.ajax({
+  url: 'https://audaxloot.github.io/xml/history.xml',
+  type: 'get',
+  dataType: 'xml',
+  success: function(data) {
+    $(data).find('historyentry').each(function() {
+      let hist_players = $(this).find('playerstring').text()
+      let hist_dkp = $(this).find('dkp').text()
+      let hist_time = $(this).find('timestamp').text()
+      let hist_reason = $(this).find('reason').text()
+
+      let new_row = `
+      <tr class="history_entry">
+        <td class="row hplayers">${hist_players}</td>
+        <td class="row hdkp">${hist_dkp}</td>
+        <td class="row htime">${hist_time}</td>
+        <td class="row hreason">${hist_reason}</td>
+      </tr>`
+
+      $("#history_table tbody").append(new_row)
+    })
+  },
+  error: function() {
+    $('#history_table').text('Failed to load data!')
   }
 })
